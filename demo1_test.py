357c961bc7f2e77db10844e8acb3228d3faf4f8e
AbhirupOzone <122984485+AbhirupOzone@users.noreply.github.com>
2023-06-30 14:13:05 +0530

    Update demo1_test.py

--- demo1_test.py
+++ demo1_test.py
@@ -2,7 +2,7 @@ import unittest
 
 class MyTestCase2(unittest.TestCase):
     def test_addition(self):
-        self.assertEqual(2 + 2, -4)
+        self.assertEqual(2 + 2, 4)
 
     def test_subtraction(self):
         self.assertEqual(5 - 3, 2)
